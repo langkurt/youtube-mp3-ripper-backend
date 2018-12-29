35765e294f45c4dac0bd9523a71c971071257be7
Kurt Lang <kurtalang@gmail.com>
2018-12-29 17:44:53 -0600

    The view function returns a valid uWSGI response

--- home.py
+++ home.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, jsonify
 from youtube_rip import download_and_convert, WRITABLE_DIR
 
 app = Flask(__name__)
@@ -9,22 +9,22 @@ def rip():
     youtube_url = request.args.get('url')
 
     if not youtube_url:
-        return {
-            'statusCode': 400,
-            'body': 'No Url Given'
+        error = {
+            'message': 'No Url Given'
         }
+        return jsonify(error), 400
 
     try:
         mp3_file_location = download_and_convert(youtube_url)
     except Exception as e:
         print(str(e))
-        return {
-            'statusCode': 500,
-            'body': "Error converting url to mp3: %s" % str(e)
+        error = {
+            'message': "Error converting url to mp3: %s" % str(e)
         }
+        return jsonify(error), 500
 
     send_from_directory(directory=WRITABLE_DIR, filename=mp3_file_location)
-    return "Hello World!", 200
+    return "Finished!", 200
 
 
 if __name__ == "__main__":
